837ccfb7a83f4b6ae5e68971485a1d8eb17ba2de
Jason Dantuma <jason@everlytic.com>
2018-11-30 15:00:47 +0200

    Fix missing answer in test

--- pushnotificationsdk/src/test/java/com/everlytic/android/pushnotificationsdk/PushSdkTest.kt
+++ pushnotificationsdk/src/test/java/com/everlytic/android/pushnotificationsdk/PushSdkTest.kt
@@ -6,8 +6,10 @@ import com.everlytic.android.pushnotificationsdk.network.EverlyticApi
 import com.everlytic.android.pushnotificationsdk.network.EverlyticHttp
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
+import okhttp3.ResponseBody
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Call
 
 class PushSdkTest {
 
@@ -15,7 +17,7 @@ class PushSdkTest {
     fun setUp() {
         mockkObject(BuildFacade)
 
-        every { BuildFacade.getPlatformVersion() } returns "testing"
+        every { BuildFacade.getPlatformVersion() } returns "[test] pversion"
         every { BuildFacade.getDeviceManufacturer() } returns "[test] manufacturer"
         every { BuildFacade.getDeviceModel() } returns "[test] model"
     }
@@ -31,18 +33,21 @@ class PushSdkTest {
         val userEmail = "test@test.com"
 
         val mockEverlyticApi = mockk<EverlyticApi>()
+        val mockCall = mockk<Call<ResponseBody>>()
         val mockHttp = mockk<EverlyticHttp>()
 
         val mockFirebaseIdFacade = mockk<FirebaseInstanceIdFacade>()
 
         coEvery { mockFirebaseIdFacade.getInstanceId() } returns "test_instance_id"
 
-        every { mockHttp.buildEverlyticApi(apiInstall, apiUsername, apiKey) } returns mockEverlyticApi
+        every { mockHttp.buildEverlyticApi(apiInstall, apiUsername, apiKey) } answers { mockEverlyticApi }
+
+        every { mockEverlyticApi.subscribe(ofType()) } returns mockCall
 
         val sdk = PushSdk(apiInstall, apiUsername, apiKey, projectId, mockHttp, mockFirebaseIdFacade)
 
         sdk.subscribeUser(userEmail)
 
-        verify(exactly = 1) { mockEverlyticApi.subscribe(any()) }
+        verify(exactly = 1) { mockEverlyticApi.subscribe(ofType()) }
     }
 }
