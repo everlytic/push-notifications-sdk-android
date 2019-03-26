2b0729f782ad2ce2ea0adebf0cbc94214b536aad
Jason Dantuma <jason@everlytic.com>
2019-03-26 10:51:59 +0200

    NT - Add Robolectric testing to reduce mocking of Android SDK in tests

--- pushnotificationsdk/src/test/java/com/everlytic/android/pushnotificationsdk/EverlyticPushTest.kt
+++ pushnotificationsdk/src/test/java/com/everlytic/android/pushnotificationsdk/EverlyticPushTest.kt
@@ -1,11 +1,9 @@
 package com.everlytic.android.pushnotificationsdk
 
 import android.app.Application
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.runner.AndroidJUnit4
 import com.everlytic.android.pushnotificationsdk.exceptions.EverlyticPushInvalidSDKConfigurationException
 import com.everlytic.android.pushnotificationsdk.exceptions.EverlyticPushNotInitialisedException
 import com.everlytic.android.pushnotificationsdk.repositories.SdkRepository
@@ -13,10 +11,12 @@ import io.mockk.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+@RunWith(AndroidJUnit4::class)
 class EverlyticPushTest {
 
     @Before
@@ -54,7 +54,7 @@ class EverlyticPushTest {
 
     @Test
     fun testInit_WithApplication_CreatesInitialisedSDK() {
-        mockApplication().let { app ->
+        getApplication().let { app ->
             mockkConstructor(PushSdk::class)
             mockkConstructor(SdkRepository::class)
 
@@ -62,6 +62,7 @@ class EverlyticPushTest {
 
             EverlyticPush.init(app)
             verify { SdkSettings.getSettings(ofType()) }
+
             clearConstructorMockk(PushSdk::class)
             clearConstructorMockk(SdkRepository::class)
         }
@@ -158,38 +159,7 @@ class EverlyticPushTest {
         return mockPushSdk
     }
 
-    private fun mockApplicationInfo() = mockk<ApplicationInfo> {
-        metaData = mockk {
-            every { getString(eq("com.everlytic.api.API_INSTALL_URL")) } returns "install_id"
-            every { getString(eq("com.everlytic.api.API_USERNAME")) } returns "api_username"
-            every { getString(eq("com.everlytic.api.API_KEY")) } returns "api_key"
-            every { getInt(eq("com.everlytic.api.PUSH_NOTIFICATIONS_PROJECT_ID"), any()) } returns 0
-        }
-    }
-
-    private fun mockPackageManager() = mockk<PackageManager> {
-        val applicationInfo = mockApplicationInfo()
-
-        every { getApplicationInfo(any(), eq(PackageManager.GET_META_DATA)) } returns applicationInfo
-    }
-
-    private fun mockApplication() = mockk<Application> {
-        val pm = mockPackageManager()
-        val appInfo = mockApplicationInfo()
-        val ctx = mockk<Context> {
-            every { registerReceiver(ofType(), ofType()) } returns Intent()
-            val sharedPreferences = mockk<SharedPreferences> {
-                every { getString(any(), any()) } answers { "[val for] ${args.first()}" }
-                every { getLong(any(), any()) } answers { 1 }
-            }
-            every { getSharedPreferences(any(), any()) } returns sharedPreferences
-        }
-
-        every { applicationInfo } returns appInfo
-        every { packageManager } returns pm
-        every { packageName } returns "packageName"
-        every { applicationContext } returns ctx
-    }
+    private fun getApplication() = ApplicationProvider.getApplicationContext<Application>()
 
     private fun mockPushSdk(): PushSdk {
         mockkConstructor(PushSdk::class)
