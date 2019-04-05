818145232d06ec8f2acc98543861f39581532a52
Jason Dantuma <jason@everlytic.com>
2019-04-05 09:15:32 +0200

    Changes to debug logger function

--- pushnotificationsdk/src/main/java/com/everlytic/android/pushnotificationsdk/EvLogger.kt
+++ pushnotificationsdk/src/main/java/com/everlytic/android/pushnotificationsdk/EvLogger.kt
@@ -5,9 +5,9 @@ import android.util.Log
 object EvLogger {
 
     fun d(tag: String, message: String? = null, throwable: Throwable? = null) {
-//        if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG || EverlyticPush.isInTestMode) {
             Log.d(tag, message, throwable)
-//        }
+        }
     }
 
     fun i(tag: String, message: String? = null, throwable: Throwable? = null) {
