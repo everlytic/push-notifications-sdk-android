c118b9a0a868a0bdb8fbba6b651a43a8de9baa7f
Jason Dantuma <jason@dantuma.co.za>
2019-06-14 10:31:05 +0200

    Add storing of fcm token hash for checking if the token changed, without leaking the whole token

--- pushnotificationsdk/src/main/java/com/everlytic/android/pushnotificationsdk/Functions.kt
+++ pushnotificationsdk/src/main/java/com/everlytic/android/pushnotificationsdk/Functions.kt
@@ -5,13 +5,14 @@ package com.everlytic.android.pushnotificationsdk
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.os.Handler
-import android.os.Looper
 import com.everlytic.android.pushnotificationsdk.models.jsonadapters.MapAdapter
 import com.everlytic.android.pushnotificationsdk.repositories.SdkRepository
 import org.json.JSONException
 import org.json.JSONObject
 import java.net.HttpURLConnection
+import java.nio.charset.Charset
+import java.security.MessageDigest
+
 
 @Suppress("UNCHECKED_CAST")
 internal fun decodeJsonMap(data: String): Map<String, String> {
@@ -104,4 +105,20 @@ internal fun updateFcmToken(sdkRepository: SdkRepository, token: String?) {
             EverlyticPush.instance?.resubscribeIfRequired()
         }
     }
+}
+
+internal fun String.getHash(): String {
+    val md = MessageDigest.getInstance("SHA-1")
+    val textBytes = this.toByteArray(Charset.forName("iso-8859-1"))
+    md.update(textBytes, 0, textBytes.size)
+    val buf = StringBuilder()
+    for (b in md.digest()) {
+        var halfbyte = (b.toInt() ushr 4) and 0x0F
+        var two_halfs = 0
+        do {
+            buf.append(if (halfbyte in 0..9) ('0'.toInt() + halfbyte).toChar() else ('a'.toInt() + (halfbyte - 10)).toChar())
+            halfbyte = b.toInt() and 0x0F
+        } while (two_halfs++ < 1)
+    }
+    return buf.toString()
 }
